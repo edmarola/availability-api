9686f861413740ceff4fa70046522d879832570c
Eduardo <eduardo110298@gmail.com>
2022-11-12 23:18:34 -0400

    Add correct error message on assertions for tests

--- tests/test_availapi.py
+++ tests/test_availapi.py
@@ -50,7 +50,11 @@ def test_sc2_empty_input(client):
     # Assert
     assert response.status_code == 422, "The status must be 422."
     assert response.is_json, "The response format must be JSON."
-    assert response.json == {"error": {}}, "The output did not match."
+    assert response.json == {
+        "errors": {
+            "json": {"_schema": ["Invalid input type. The array is empty."]}
+        }
+    }, "The output did not match."
 
 
 def test_sc3_from_to_inverted(client):
@@ -78,7 +82,18 @@ def test_sc3_from_to_inverted(client):
     # Assert
     assert response.status_code == 422, "The status must be 422."
     assert response.is_json, "The response format must be JSON."
-    assert response.json == {"error": {}}, "The output did not match."
+    assert response.json == {
+        "errors": {
+            "json": {
+                "0": {
+                    "_schema": [
+                        "The `from` field must be a past date"
+                        " in regards to the `to` field."
+                    ]
+                }
+            }
+        }
+    }, "The output did not match."
 
 
 def test_sc4_range_missing_keys(client):
@@ -103,7 +118,17 @@ def test_sc4_range_missing_keys(client):
     # Assert
     assert response.status_code == 422, "The status must be 422."
     assert response.is_json, "The response format must be JSON."
-    assert response.json == {"error": {}}, "The output did not match."
+    assert response.json == {
+        "errors": {
+            "json": {
+                "0": {"to": ["Missing data for required field."]},
+                "1": {
+                    "cc": ["Missing data for required field."],
+                    "from": ["Missing data for required field."],
+                },
+            }
+        }
+    }, "The output did not match."
 
 
 def test_sc5_missing_body(client):
@@ -117,4 +142,53 @@ def test_sc5_missing_body(client):
     # Assert
     assert response.status_code == 422, "The status must be 422."
     assert response.is_json, "The response format must be JSON."
-    assert response.json == {"error": {}}, "The output did not match."
+    assert response.json == {
+        "errors": {"json": {"_schema": ["Invalid input type."]}}
+    }, "The output did not match."
+
+
+def test_sc6_field_values_format_error(client):
+    """
+    Scenario 6: Field values with format error
+    Given all the required field with format errors, an error is returned.
+    """
+    # Arrange
+    request_body = [
+        {"from": "", "to": "2022-05-02T17:00:00.000+08:00", "cc": "ZZ"}
+    ]
+
+    # Act
+    response = client.post(ENDPOINT_URL, json=request_body)
+
+    # Assert
+    assert response.status_code == 422, "The status must be 422."
+    assert response.is_json, "The response format must be JSON."
+    assert response.json == {
+        "errors": {
+            "json": {
+                "0": {
+                    "cc": [
+                        "Must be one of: AF, AL, DZ, AS, AD, AO, AI, AG, AR, "
+                        "AM, AW, AU, AT, AZ, BH, BD, BB, BY, BE, BZ, BJ, BM, "
+                        "BT, BO, BA, BW, BR, VG, BN, BG, BF, BI, CV, KH, CM, "
+                        "CA, KY, CF, TD, CL, CN, CO, KM, CG, CD, CK, CR, CI, "
+                        "HR, CU, CW, CY, CZ, DK, DJ, DM, DO, TL, EC, EG, SV, "
+                        "GQ, ER, EE, ET, FK, FO, FJ, FI, FR, PF, GA, GM, GE, "
+                        "DE, GH, GI, GR, GL, GD, GU, GT, GG, GN, GW, GY, HT, "
+                        "HN, HK, HU, IS, IN, ID, IR, IQ, IE, IM, IL, IT, JM, "
+                        "JP, JE, JO, KZ, KE, KI, XK, KW, KG, LA, LV, LB, LS, "
+                        "LR, LY, LI, LT, LU, MO, MG, MW, MY, MV, ML, MT, MH, "
+                        "MQ, MR, MU, YT, MX, FM, MD, MC, MN, ME, MS, MA, MZ, "
+                        "MM, NA, NR, NP, NL, NC, NZ, NI, NE, NG, KP, MK, MP, "
+                        "NO, OM, PK, PW, PA, PG, PY, PE, PH, PL, PT, PR, QA, "
+                        "RE, RO, RU, RW, SH, KN, LC, MF, PM, VC, WS, SM, ST, "
+                        "SA, SN, RS, SC, SL, SG, SX, SK, SI, SB, SO, ZA, KR, "
+                        "SS, ES, LK, BL, SD, SR, SE, CH, SY, TW, TJ, TZ, TH, "
+                        "BS, TG, TO, TT, TN, TR, TM, TC, TV, VI, UG, UA, AE, "
+                        "GB, US, UY, UZ, VU, VA, VE, VN, WF, YE, ZM, ZW, SZ."
+                    ],
+                    "from": ["Not a valid datetime."],
+                }
+            }
+        }
+    }, "The output did not match."
